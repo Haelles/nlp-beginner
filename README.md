d444d19a438691dc86674d87261a20c6a1281f36
haelles <haelle@buaa.edu.cn>
2022-05-21 17:56:54 +0800

    learning+preprocessing

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # nlp-beginner
+22级新手任务
+
+## Environment
+### 版本
+torch 1.5.1
+torchtext 0.6.0
+torchvision 0.6.0
+spacy 3.3.0
+
+### torchtext ValueError
+torchtext—ValueError: Fan in and fan out can not be computed for tensor with fewer than 2 dimensions
+可参考https://blog.csdn.net/qq_23262411/article/details/100173224?spm=1001.2101.3001.6650.1&utm_medium=distribute.pc_relevant.none-task-blog-2%7Edefault%7ECTRLIST%7Edefault-1-100173224-blog-114952212.pc_relevant_aa&depth_1-utm_source=distribute.pc_relevant.none-task-blog-2%7Edefault%7ECTRLIST%7Edefault-1-100173224-blog-114952212.pc_relevant_aa&utm_relevant_index=2
+
---
+++ preprocessing/preprocessing.py
@@ -0,0 +1,29 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+
+def generate_csv(data_path, train_path, test_path, test_size=0.2):
+    train=pd.read_csv(train_path, sep='\t')
+    test = pd.read_csv(test_path, sep='\t')
+    
+    origin_data = train[['PhraseId', 'SentenceId', 'Phrase']]
+    origin_label = train[['Sentiment']]
+    train_data, val_data, train_label, val_label = train_test_split(
+                                            origin_data, origin_label, test_size=0.2, random_state=0)
+    
+    train_final = pd.merge(train_data, train_label, left_index=True, right_index=True)
+    val_final = pd.merge(val_data, val_label, left_index=True, right_index=True)
+    print('train_final: {len}'.format(len=len(train_final)))
+    print('val_final: {len}'.format(len=len(val_final)))
+    print('test_final: {len}'.format(len=len(test)))
+    
+    train_final.to_csv(data_path + 'train.csv', index=False)
+    val_final.to_csv(data_path + 'val.csv', index=False)
+    test.to_csv(data_path + 'test.csv', index=False) 
+
+
+if __name__ == '__main__':
+    data_path = 'data/sentiment-analysis-on-movie-reviews/'
+    train_path = 'data/sentiment-analysis-on-movie-reviews/train.tsv'
+    test_path = 'data/sentiment-analysis-on-movie-reviews/test.tsv'
+    generate_csv(data_path, train_path, test_path)
