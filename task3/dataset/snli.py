6cca4ba4aff618e67a8b9c5724215f2014a14f54
haelles <haelle@buaa.edu.cn>
2022-06-08 01:45:35 +0800

    iterator part done; need to consider pad_sequence later

--- task3/dataset/snli.py
+++ task3/dataset/snli.py
@@ -9,29 +9,46 @@ import jsonlines
 
 class SnliDataset(data.Dataset):
     def __init__(self, file_path, text_field, label_field):
-        fields = {
+        self.label2idx = {
+            'contradiction': 0,
+            'entailment': 1,
+            'neutral': 2
+        }
+
+        # data.Example.fromdict()方法需要dict{key: tuple()}形式的fields
+        self.rename_fields = {
                 'sentence1': ('premise', text_field),  # data.Example.fromdict里面涉及name, field = val，需要赋一个名字
                 'sentence2': ('hypothesis',text_field), 
                 'gold_label': ('label', label_field)
             }
+        # init方法需要list[tuple()]形式的fields
+        self.fields = [
+            ('premise', text_field),
+            ('hypothesis',text_field),
+            ('label', label_field)
+        ]
 
-        examples = []
+        self.examples = []
         with open(file_path, 'r+', encoding='utf-8') as f:
             cnt = 0
             total_cnt = 0
             print('read data from {path}'.format(path=file_path))
             for item in jsonlines.Reader(f):
                 total_cnt += 1
-                if item['gold_label'] == '-':
-                    print(item['pairID'])
-                    print(item['sentence1'])
+                if item['gold_label'] == '-':  # 过滤没有标签也即五个人没有达成共识的
+                    # print(item['pairID'])
+                    # print(item['sentence1'])
                     continue
-                examples.append(data.Example.fromdict(item, fields))
+                item['gold_label'] = self.label2idx[item['gold_label']]  # 转换成数字，用于loss计算
+                self.examples.append(data.Example.fromdict(item, self.rename_fields))
                 cnt += 1
             print('data length:{length}'.format(length=cnt))
             print('total file length:{length}'.format(length=total_cnt))
+            # print(self.examples[0].premise)
+            # print(self.examples[0].hypothesis)
+            # print(self.examples[0].label)
         
-        super(SnliDataset, self).__init__(examples=examples, fields=fields)
+        super(SnliDataset, self).__init__(examples=self.examples, fields=self.fields)
         
 
 if __name__ == '__main__':
--- task3/utils/iterators.py
+++ task3/utils/iterators.py
@@ -10,34 +10,38 @@ sys.path.append(".")
 from dataset.snli import SnliDataset
 
 
-def build_iterator(file_path, text_field=None, batch_size=1, shuffle=True, spcay_load='en_core_web_sm', vector_file_name='glove.840B.300d.txt', train_tag='train', device=-1):
+def build_iterator(file_path, batch_size=1, device=-1, is_train=True, text_field=None, spcay_load='en_core_web_sm', vector_file_name='glove.840B.300d.txt'):
     if text_field is None:
         nlp = spacy.load(spcay_load)
         tokenize = lambda x: [tok.text for tok in nlp.tokenizer(x)]
         text_field = data.Field(sequential=True, tokenize=tokenize, lower=True)
-    label_field = data.Field(sequential=False, use_vocab=False)
+    label_field = data.Field(sequential=False, use_vocab=False, is_target=False)  # 用data.LabelField应该也可以
 
-    cur_dataset = SentimentDataset(file_path, text_field, label_field, train_tag)
+    cur_dataset = SnliDataset(file_path, text_field, label_field)
 
-    vectors = Vectors(name=vector_file_name)
+    vectors = Vectors(name=vector_file_name, cache='../.vector_cache/')
     vectors.unk_init = nn.init.xavier_uniform_
 
     text_field.build_vocab(cur_dataset, vectors=vectors)
-    if train_tag == 'train':
-        cur_iterator = BucketIterator(dataset=cur_dataset, batch_size=batch_size, device=device, sort_key=lambda x: len(x.Phrase), shuffle=True, sort_within_batch=False, repeat=False)
+    if is_train:
+        cur_iterator = BucketIterator(dataset=cur_dataset, batch_size=batch_size, device=device, sort_key=lambda x: len(x.premise) + len(x.hypothesis), shuffle=True, sort_within_batch=False, repeat=False)
     else:
         cur_iterator = Iterator(dataset=cur_dataset, batch_size=batch_size, device=device, train=False, sort=False, repeat=False)
     return cur_iterator
 
 
 if __name__ == '__main__':
-    file_path = 'data/sentiment-analysis-on-movie-reviews/test.csv'
-    batch_size = 2
+    # file_path = '../data/snli_1.0/snli_1.0_test.jsonl'
+    file_path = '../data/snli_1.0/snli_1.0_dev.jsonl'
+    batch_size = 32
     is_train = False
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     
     cur_iterator = build_iterator(file_path, batch_size=batch_size, device=device, is_train=is_train)
-    # import pdb
-    # pdb.set_trace()
+    import pdb
+    pdb.set_trace()
+    # dir(batch): 'batch_size', 'dataset', 'fields', 'fromvars', 'hypothesis', 'input_fields', 'label', 'premise', 'target_fields' etc.
+    # batch.hypothesis.shape torch.Size([16, 32]) torch.Size([20, 32])
+    # p batch.premise.shape torch.Size([28, 32]) torch.Size([34, 32])
     batch = next(iter(cur_iterator))
     
