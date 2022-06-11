bb8b46b16a36c2c5bb6ca4c0b4854d54ed12892d
haelles <haelle@buaa.edu.cn>
2022-06-11 16:30:49 +0800

    init_weight debug

--- task3/model/ESIM.py
+++ task3/model/ESIM.py
@@ -48,6 +48,8 @@ class ESIM(nn.Module):
             nn.Linear(hidden_size, num_class)
         )
 
+        self.apply(_init_esim_weights)
+
 
     def forward(self, premise_sequence, premise_length, hypothesis_sequence, hypothesis_length):
         # premise, hypothesis [b, l]
@@ -107,6 +109,30 @@ class ESIM(nn.Module):
         prediction = nn.functional.softmax(logits, dim=1)
 
         return logits, prediction
+
+
+def _init_esim_weights(module):
+    """
+    Initialise the weights of the ESIM model.
+    """
+    if isinstance(module, nn.Linear):
+        nn.init.xavier_uniform_(module.weight.data)
+        nn.init.constant_(module.bias.data, 0.0)
+
+    elif isinstance(module, nn.LSTM):
+        nn.init.xavier_uniform_(module.weight_ih_l0.data)
+        nn.init.orthogonal_(module.weight_hh_l0.data)
+        nn.init.constant_(module.bias_ih_l0.data, 0.0)
+        nn.init.constant_(module.bias_hh_l0.data, 0.0)
+        hidden_size = module.bias_hh_l0.data.shape[0] // 4
+        module.bias_hh_l0.data[hidden_size:(2*hidden_size)] = 1.0
+
+        if (module.bidirectional):
+            nn.init.xavier_uniform_(module.weight_ih_l0_reverse.data)
+            nn.init.orthogonal_(module.weight_hh_l0_reverse.data)
+            nn.init.constant_(module.bias_ih_l0_reverse.data, 0.0)
+            nn.init.constant_(module.bias_hh_l0_reverse.data, 0.0)
+            module.bias_hh_l0_reverse.data[hidden_size:(2*hidden_size)] = 1.0
 
 
 if __name__ == '__main__':
